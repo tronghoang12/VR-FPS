b0addd156605defd0495c971f39e3f797084abf9
Vũ Huy Công <111194654+vhc892@users.noreply.github.com>
2024-07-26 14:14:54 +0700

    vhc

---
+++ Assets/Script/TargetDummy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDummy : MonoBehaviour
+{
+    [SerializeField] private Animator dummyAnimator;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Weapon"))
+        {
+            dummyAnimator.SetTrigger("Die");
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+    public void ActivateDummy()
+    {
+        dummyAnimator.SetTrigger("Activate");
+    }
+}
